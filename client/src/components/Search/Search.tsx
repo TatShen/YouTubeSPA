c89feed1cb58cd617aa9238c750bc81b44a25d27
Tat Shen <tatshen97@gmail.com>
2024-11-07 14:43:07 +0100

    -add tooltip

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -7,37 +7,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { IRootSate } from "../../redux/store";
 import { addRequest, getVideos } from "../../redux/actions/videoAction";
 import { searchApi } from "../../services/Search.service";
+import { Tooltip } from "antd";
 
 interface ISearchProps {
   setIsModalActive: (value: boolean) => void;
 }
 
-export const Search: React.FC<ISearchProps> = ({
-  setIsModalActive
-}) => {
-  const [search, setSearch] = useState("")
+export const Search: React.FC<ISearchProps> = ({ setIsModalActive }) => {
+  const [search, setSearch] = useState("");
+  const [isFavorite, setIsFavorite] = useState<boolean | undefined>(false);
   const dispatch = useDispatch();
-  const {videos, request} = useSelector((state: IRootSate) => state.video);
+  const { videos, request } = useSelector((state: IRootSate) => state.video);
+  const { user } = useSelector((state: IRootSate) => state.user);
 
-  useEffect(()=> {
-    if(request){
-      setSearch(request)
+  useEffect(() => {
+    const isFavorite = user?.requests.some((item) => item.request === search);
+    setIsFavorite(isFavorite);
+  }, [search, user?.requests]);
+
+  useEffect(() => {
+    if (request) {
+      setSearch(request);
     }
-  },[request])
+  }, [request]);
 
   const handlerChangeRequest = (value: string) => {
-    setSearch(value)
-  }
+    setSearch(value);
+  };
 
   const handlerSearch = async () => {
     const results = await searchApi.getVideos(search);
     if (results) {
       dispatch(getVideos(results));
-      dispatch(addRequest(search))
+      dispatch(addRequest(search));
     }
   };
-  
-  
+
   return (
     <div className={videos?.length ? style.search_on_top : style.search}>
       <h1 className={style.search_title}>Поиск видео</h1>
@@ -49,10 +54,28 @@ export const Search: React.FC<ISearchProps> = ({
           className={style.search_input}
           handler={(e) => handlerChangeRequest(e.target.value)}
         />
-        {videos?.length && (
-          <div className={style.icon} onClick={() => setIsModalActive(true)}>
-            <IoHeartOutline />
-          </div>
+        {search && (
+          <Tooltip
+            placement="bottom"
+            title={
+              <div className={style.tooltip}>
+                <span className={style.tooltipTitle}>
+                  Поиск сохранён в разделе «Избранное»
+                </span>
+                <a href="/favorite" className={style.tooltipLink}>
+                  Перейти в избранное
+                </a>
+              </div>
+            }
+            arrow={true}
+            color={"#fcfafaf1"}
+            open={isFavorite}
+            trigger={"hover"}
+          >
+            <div className={style.icon} onClick={() => setIsModalActive(true)}>
+              <IoHeartOutline color={isFavorite ? "#1890FF" : "#d1d1d1"} />
+            </div>
+          </Tooltip>
         )}
         <Button
           type={"button"}
